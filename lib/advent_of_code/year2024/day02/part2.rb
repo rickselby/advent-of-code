4f5ead1f9e1eb6ca9b310e29fd9ebc07993e48df
Rick Selby <rick@selby-family.co.uk>
2024-12-03 14:33:34 +0000

    Complete 2024 Day 2, Part 2

---
+++ lib/advent_of_code/year2024/day02/part2.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module AdventOfCode
+  module Year2024
+    module Day02
+      # https://adventofcode.com/2024/day/2
+      class Part2 < Part1
+        def result
+          reports.count { |r| safe_with_damping? r }
+        end
+
+        private
+
+        def safe_with_damping?(report)
+          return true if safe? report
+
+          report.count.times do |c|
+            return true if safe?(report.reject.with_index { |_, i| i == c })
+          end
+
+          false
+        end
+      end
+    end
+  end
+end
