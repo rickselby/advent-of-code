eb35e8dbf0cdcb04495ad6f186fd418a523a8dc4
Rick Selby <rick@selby-family.co.uk>
2024-12-10 14:30:12 +0000

    Complete 2015 Day 6, Part 2

---
+++ lib/advent_of_code/year2015/day06/part2.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module AdventOfCode
+  module Year2015
+    module Day06
+      # https://adventofcode.com/2015/day/6
+      class Part2 < Part1
+        def result
+          lines.each { |l| parse l }
+          @map.sum { |l| l.compact.sum }
+        end
+
+        private
+
+        def parse(line)
+          range = range line
+          if line.start_with?("turn on")
+            change range, :+, 1
+          elsif line.start_with?("turn off")
+            change range, :-, 1
+          elsif line.start_with?("toggle")
+            change range, :+, 2
+          else
+            raise "what"
+          end
+        end
+
+        def change(range, operator, value)
+          for_range(range) do |x, y|
+            @map[x][y] = 0 if @map[x][y].nil?
+            @map[x][y] = @map[x][y].public_send(operator, value)
+            @map[x][y] = 0 if @map[x][y].negative?
+          end
+        end
+      end
+    end
+  end
+end
