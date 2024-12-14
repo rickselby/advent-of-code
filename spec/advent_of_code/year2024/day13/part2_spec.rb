093c07d1ad8e91592ea185c93e63e958158063d3
Rick Selby <rick@selby-family.co.uk>
2024-12-14 07:35:37 +0000

    Remove unnecessary tests

--- spec/advent_of_code/year2024/day13/part2_spec.rb
+++ spec/advent_of_code/year2024/day13/part2_spec.rb
@@ -1,15 +1,3 @@
 # frozen_string_literal: true
 
-describe AdventOfCode::Year2024::Day13::Part2 do
-  describe "result" do
-    subject { described_class.new(data).result }
-
-    let(:data) do
-      <<~DATA
-        # data goes here
-      DATA
-    end
-
-    it { is_expected.to eq :something }
-  end
-end
+# No samples given
